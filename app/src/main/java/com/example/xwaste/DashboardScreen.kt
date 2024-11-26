a79227ed488314113d9c8df05d6ac5c642487579
Mark Mutinda <122874277+Mutinda-Mark@users.noreply.github.com>
2024-11-26 12:19:14 +0300

    Import correction

--- app/src/main/java/com/example/xwaste/DashboardScreen.kt
+++ app/src/main/java/com/example/xwaste/DashboardScreen.kt
@@ -1,45 +1,5 @@
 package com.example.xwaste
 
-import android.content.Intent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import com.google.firebase.auth.FirebaseAuth
-import coil.compose.rememberAsyncImagePainter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.runtime.*
-import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.rememberNavController
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Close
-import kotlinx.coroutines.launch
 
 
 @OptIn(ExperimentalMaterial3Api::class)
