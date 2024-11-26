e7e8a6860f3efdbd03fab1b25a0574bc2ef7ebba
Mark Mutinda <122874277+Mutinda-Mark@users.noreply.github.com>
2024-11-26 12:17:07 +0300

    Update AccountScreen.kt

--- app/src/main/java/com/example/xwaste/AccountScreen.kt
+++ app/src/main/java/com/example/xwaste/AccountScreen.kt
@@ -1,6 +1,31 @@
 package com.example.xwaste
 
-
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.xwaste.ui.theme.XWasteTheme
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import android.os.Bundle
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import kotlinx.coroutines.launch
 
 
 class AccountActivity : ComponentActivity() {
